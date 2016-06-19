0807d74f7ca14d99d267ae2408a1bab7c78a8e6d
Vasiliy Sabadazh <vsabadazh@gmail.com>
2016-06-19 14:29:08 +0300

    adjusted focus position; no unfocus

--- static/vasya.js
+++ static/vasya.js
@@ -1,18 +1,12 @@
 function unfocusMap()
 {
-    gisMap.flyTo({
-        lat: 55.752314, 
-        lng: 37.623483,
-        zoom: 1
-    });
 }
-
 
 function focusMap(lat, lng)
 {
     gisMap.flyTo({
         lat: lat, 
-        lng: lng - 0.0001,
+        lng: lng - 0.001,
         zoom: 15,
         minZoom: 10        
     });
