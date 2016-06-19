432e34e3ee8ea516063e1db945713d11b5d703a1
Maksim <maksim@MacBook-Pro-Maksim.local>
2016-06-19 14:24:11 +0300

    card

--- static/maksim.js
+++ static/maksim.js
@@ -18,6 +18,35 @@ function getSchoolInfo(school) {
         '</div>';
 }
 
+function getNoiseInfo(noise) {
+    return '<div class="card__line">' +
+        '<div class="card__line-cut">' +
+        '<div class="card__line-desc">' +
+        noise.reason +
+        '</div>' +
+        '</div>' +
+        '</div>';
+}
+
+function getCommoditiInfo(comm){
+    return '<div class="card__line">' +
+        '<div class="card__line-cut">' +
+        '<div class="card__line-desc">' +
+        comm.name + 'адрес <span>'+comm.address+'<span>'+
+        '</div>' +
+        '</div>' +
+        '</div>';
+}
+function getHeader(name){
+    return '<div class="card__line">' +
+        '<div class="card__line-cut">' +
+        '<div class="card__line-desc">' +
+        '<b>'+ name + '</b>' +
+        '</div>' +
+        '</div>' +
+        '</div>';
+}
+
 function nearestTimeFormat(time) {
     if (!time) {
         return "меньше минуты"
@@ -49,6 +78,7 @@ function fillRatingCard(ratingData) {
 
 
     if (schoolsArr.length) {
+        $(".js-education-summary").parent().next(".fa").show();
         $(".js-education-summary").html(["Рядом есть несколько хороших школ"]);
 
 
@@ -57,13 +87,54 @@ function fillRatingCard(ratingData) {
         }).join(""));
     } else {
         $(".js-education-summary").html(["До ближайшей школы идти не менее 20 мин"]);
-        $(".js-education-summary").parent().next(".fa").hide();
+        $(".js-education-summary").parent().next(".fa").removeClass("fa-arrow-up").addClass("fa-arrow-down").hide();
+        $(".js-card-section-edu").html("");
     }
 
     $(".js-card-section-metro").append(metroHtml);
 
     if(ecology.plants && ecology.plants.length) {
-        $(".js-ecology-summary").html(["По данным экологической службы качество озеленения района — ",ecology.plants[0].plantQuality, " из 100"])
+        $(".js-ecology-summary").html(["по данным экологической службы качество озеленения района — ",ecology.plants[0].plantQuality, " из 100"])
+    }else{
+        $(".js-ecology").hide();
     }
+
+    if(ecology.noises && ecology.noises.length) {
+        $(".js-noise").find(".fa").show();
+        $(".js-noise-summary").html(["есть превышения нормативов допустимого уровня шума"]);
+        $(".js-card-section-noise").append(ecology.noises.map(function (i) {
+            return getNoiseInfo(i);
+        }).join(""));
+    }else{
+        $(".js-noise-summary").html(["нет превышений нормативов допустимого уровня шума"]);
+        $(".js-noise").find(".fa").removeClass("fa-arrow-up").addClass("fa-arrow-down").hide();
+        $(".js-card-section-noise").html("");
+    }
+
+    var comodoties = ratingData.commodities;
+
+    $(".js-commodities-summary").html(["В 5 минутах хотьбы расположено. Банков:",comodoties.banks.length," аптек:",comodoties.chemists.length," ресторанов:",comodoties.restaurants.length," супермаркетов:",comodoties.supermarkets.length])
+
+    $(".js-card-section-commodities").append(getHeader("Банки"));
+    $(".js-card-section-commodities").append(comodoties.banks.map(function (i) {
+        return getCommoditiInfo(i);
+    }).join(""));
+
+
+    $(".js-card-section-commodities").append(getHeader("Аптеки"));
+    $(".js-card-section-commodities").append(comodoties.chemists.map(function (i) {
+        return getCommoditiInfo(i);
+    }).join(""));
+
+    $(".js-card-section-commodities").append(getHeader("Рестораны"));
+    $(".js-card-section-commodities").append(comodoties.restaurants.map(function (i) {
+        return getCommoditiInfo(i);
+    }).join(""));
+
+    $(".js-card-section-commodities").append(getHeader("Супермаркеты"));
+    $(".js-card-section-commodities").append(comodoties.supermarkets.map(function (i) {
+        return getCommoditiInfo(i);
+    }).join(""));
+
 
 }
